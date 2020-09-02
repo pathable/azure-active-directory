07acd3faf1406a4e359c5c2308d21eaeca02dbb2
Igor Fontana <igor193@gmail.com>
2020-09-02 12:57:31 -0300

    Changing the package versions

--- package.js
+++ package.js
@@ -1,17 +1,18 @@
 Package.describe({
   summary: 'Azure Active Directory OAuth flow',
-  version: '1.2.0-beta.1',
-  name: 'sornii:azure-active-directory',
-  git: 'https://github.com/Sornii/azure-active-directory',
+  version: '1.3.1',
+  name: 'pathable:azure-active-directory',
 });
 
 Package.onUse(api => {
-  api.use('oauth2@1.2.0', ['client', 'server']);
-  api.use('oauth@1.2.1', ['client', 'server']);
-  api.use('http@1.3.0', ['server']);
-  api.use('service-configuration@1.0.11', ['client', 'server']);
-  api.use('random@1.0.10', 'client');
-  api.use('accounts-base@1.4.2', ['client', 'server']);
+  api.versionsFrom('1.11');
+
+  api.use('oauth2', ['client', 'server']);
+  api.use('pathable:oauth', ['client', 'server']);
+  api.use('http', ['server']);
+  api.use('service-configuration', ['client', 'server']);
+  api.use('random', 'client');
+  api.use('accounts-base', ['client', 'server']);
 
   api.export('AzureAd');
 
